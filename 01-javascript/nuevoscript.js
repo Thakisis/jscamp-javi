6ec6c0fac235a9f110b1d1450dfa2289617bb969
Thakisis <sub722@gmail.com>
2025-10-14 22:52:14 +0200

    cambio de javascript

---
+++ 01-javascript/nuevoscript.js
@@ -0,0 +1,29 @@
+const filters = {}
+const filtersContainer = document.getElementById('filters-container');
+
+filtersContainer.addEventListener('click', (event) => {
+    const select = event.target.closest('select');
+    const name = select?.name;
+
+    if (!select || !name) return;
+    const value = select.value;
+    filters[name] = value;
+    if (!value) delete filters[name];
+    console.log(filters);
+    renderfilters(filters);
+
+});
+
+function renderfilters(filters) {
+    const offers = document.querySelectorAll('.job-listing-card');
+    offers.forEach((offer) => {
+        const data = offer.dataset;
+        const isMatch = Object.entries(filters).reduce((match, [key, value]) => {
+            const matchValue = data[key].split(",").includes(value);
+            return match && matchValue;
+        }, true);
+
+        offer.style.display = isMatch ? 'block' : 'none';
+    });
+
+}
