93e74abe2e3756874ff75e56e4f910a87ee42c8f
Thakisis <sub722@gmail.com>
2025-10-16 17:28:47 +0200

    añadido filtrado
    test

---
+++ 01-javascript/data/textos.js
@@ -0,0 +1,52 @@
+
+export const locations = [
+    { key: "remoto", name: "Remoto" },
+    { key: "cdmx", name: "Ciudad de México" },
+    { key: "gdl", name: "Guadalajara" },
+    { key: "mty", name: "Monterrey" },
+    { key: "barc", name: "Barcelona" },
+    { key: "madrid", name: "Madrid" }
+];
+export const experienceLevels = [
+    { key: "junior", name: "Junior" },
+    { key: "mid", name: "Semi-Senior" },
+    { key: "senior", name: "Senior" }
+];
+export const technologies = [
+    { key: "aws", name: "Amazon Web Services (AWS)" },
+    { key: "azure", name: "Microsoft Azure" },
+    { key: "backend", name: "Desarrollo Backend" },
+    { key: "cybersecurity", name: "Ciberseguridad" },
+    { key: "frontend", name: "Desarrollo Frontend" },
+    { key: "fullstack", name: "Desarrollo Full Stack" },
+    { key: "gcp", name: "Google Cloud Platform (GCP)" },
+    { key: "it", name: "Tecnologías de la Información (TI)" },
+    { key: "javascript", name: "JavaScript" },
+    { key: "kotlin", name: "Kotlin" },
+    { key: "mobile", name: "Desarrollo Móvil" },
+    { key: "networking", name: "Redes y Conectividad" },
+    { key: "nodejs", name: "Node.js" },
+    { key: "python", name: "Python" },
+    { key: "r", name: "R (Lenguaje de Programación)" },
+    { key: "react", name: "React" },
+    { key: "sql", name: "SQL" },
+    { key: "swift", name: "Swift" },
+    { key: "uxui", name: "Diseño UX/UI" },
+    { key: "html", name: "Maquetación HTML" },
+    { key: "css", name: "Maquetación CSS" },
+    { key: "django", name: "Django" },
+    { key: "python", name: "Python" },
+    { key: "r", name: "R (Lenguaje de Programación)" },
+    { key: "react", name: "React" },
+    { key: "sql", name: "SQL" },
+    { key: "swift", name: "Swift" },
+    { key: "angular", name: "Angular" },
+    { key: "nodejs", name: "Node.js" },
+    { key: "mongodb", name: "MongoDB" },
+];
+export const contractTypes = [
+    { key: "fulltime", name: "Tiempo Completo" },
+    { key: "parttime", name: "Medio Tiempo" },
+    { key: "contract", name: "Contrato por Proyecto" },
+    { key: "internship", name: "Pasantía" }
+];
--- 01-javascript/nuevoscript.js
+++ 01-javascript/nuevoscript.js
@@ -1,3 +1,6 @@
+import { offerCard, tagSpan } from './data/html-functions.js';
+
+import jobListings from './data/ofertas.js';
 const filters = {}
 const filtersContainer = document.getElementById('filters-container');
 
@@ -13,16 +16,36 @@ filtersContainer.addEventListener('change', (event) => {
     renderfilters(filters);
 
 });
-
+renderOffers(jobListings);
 function renderfilters(filters) {
     const offers = document.querySelectorAll('.job-listing-card');
-    offers.forEach((offer) => {
-        const data = offer.dataset;
-        const isMatch = Object.entries(filters).reduce((match, [key, value]) => {
-            const matchValue = data[key].split(",").includes(value);
-            return match && matchValue;
-        }, true);
 
-        offer.style.display = isMatch ? 'block' : 'none';
-    });
+    const jobsfilter = jobListings.filter(job =>
+        Object.entries(filters).reduce((match, [key, value]) => {
+            const matchValue = job[key].includes(value);
+            return match && matchValue;
+        }, true)
+    )
+    console.log("filters", jobsfilter);
+
+    renderOffers(jobsfilter);
 }
+function renderOffers(jobs) {
+    const jobsContainer = document.getElementById('jobs');
+    const nodes = jobs.map(job => renderOffer(job));
+    jobsContainer.replaceChildren(...nodes);
+}
+
+
+function renderOffer({ tag, technology, ...job }) {
+
+    const jobCard = offerCard(job)
+    const offerNode = new DOMParser().parseFromString(jobCard, 'text/html').body.firstChild;
+    const tagsContainger = offerNode.querySelector('#tags');
+    const tags = technology.map(tagSpan)
+    const tagsNode = tags.map(tag => new DOMParser().parseFromString(tag, 'text/html').body.firstChild);
+    tagsContainger.append(...tagsNode);
+    return offerNode;
+
+}
+
